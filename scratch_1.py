fad191a10ed212561ac006272051e5c639cd602a
abdirizak kulmiye <kulmie73@hotmail.com>
2020-05-19 22:12:59 -0700

    commit python file

---
+++ scratch_1.py
@@ -0,0 +1,19 @@
+for FIZZBUZZ in range (100):
+    if FIZZBUZZ % 3 == 0 and FIZZBUZZ % 5 ==0:
+        print('FUZZBUZZ')
+    elif FIZZBUZZ %3 ==0:
+        print('FIZZ')
+    elif FIZZBUZZ % 5 == 0:
+        print('BUZZ')
+        print('BUZZ')
+        print('FIZZBUZZ')
+
+
+
+
+
+
+
+
+
+
